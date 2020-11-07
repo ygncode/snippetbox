8ef28f3b63ecefcd3f1809f085a67560d0528c66
Set Kyar Wa Lar (Universe) <setkyar16@gmail.com>
2020-11-07 23:09:35 +0630

    2.9 serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-    log.Println("Starting server on :4000", mux)
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	log.Println("Starting server on :4000", mux)
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
